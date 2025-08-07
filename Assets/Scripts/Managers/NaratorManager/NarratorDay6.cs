2efdc7581af8f41f0ceaf65445a26477361ad5ef
princenizroh <princenizroh7@gmail.com>
2025-08-08 01:32:16 +0800

    Narrator Day 6 done but not fixe

--- Assets/Scripts/Managers/NaratorManager/NarratorDay6.cs
+++ Assets/Scripts/Managers/NaratorManager/NarratorDay6.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class NarratorDay6 : NarratorBase
 {
-    // Day 6 - Baby Blues Escalation
-    // Mother starts showing signs of deeper psychological issues
-    
     [System.Obsolete]
     protected override IEnumerator PlayAfternoonSequence()
     {
@@ -24,7 +21,6 @@ public class NarratorDay6 : NarratorBase
         FadeOpenEyes();
         yield return new WaitForSeconds(1f);
 
-        // Seq1 Keraguan
         bool seq1Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq1Keraguan", 
             () => { seq1Complete = true; });
@@ -44,27 +40,15 @@ public class NarratorDay6 : NarratorBase
         TimeManager.instance.TimeOfDay = 18.0f;
         SetCharacterSpawn(CharacterType.Baby, 0);
         SetCharacterSpawn(CharacterType.Mother, 0);
-        uiElements.narratorText.gameObject.SetActive(true);
         
         yield return new WaitForSeconds(1f);
-        uiElements.narratorText.text = "Sore Hari\nHeran";
-        yield return new WaitForSeconds(2f);
-        uiElements.narratorText.gameObject.SetActive(false);
         
-        FadeOpenEyes();
-        yield return new WaitForSeconds(1f);
-        
-        // Mother comes to check
-        yield return StartCoroutine(MoveAgentToMovementPosition(CharacterType.Mother, 0));
-        
-        // Seq2 Heran
         bool seq2Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq2Heran", 
             () => { seq2Complete = true; });
         yield return new WaitUntil(() => seq2Complete);
         
-        FadeCloseEyes(); 
-        yield return new WaitForSeconds(2f);
+       yield return new WaitForSeconds(2f);
         
         GoToNextTimeOfDay();
     }
@@ -77,17 +61,13 @@ public class NarratorDay6 : NarratorBase
         TimeManager.instance.TimeOfDay = 1.0f;
         SetCharacterSpawn(CharacterType.Baby, 0);
         SetCharacterSpawn(CharacterType.Mother, 0);
-        uiElements.narratorText.gameObject.SetActive(true);
+        SetCharacterSpawn(CharacterType.Ghost, 0);
         
         yield return new WaitForSeconds(1f);
-        uiElements.narratorText.text = "Malam Hari\nSosok Mendekat";
-        yield return new WaitForSeconds(2f);
-        uiElements.narratorText.gameObject.SetActive(false);
         
         FadeOpenEyes();
         yield return new WaitForSeconds(1f);
         
-        // Seq3 Gangguan
         bool seq3Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq3Gangguan", 
             () => { seq3Complete = true; });
@@ -95,7 +75,6 @@ public class NarratorDay6 : NarratorBase
         
         yield return new WaitForSeconds(1f);
         
-        // Seq4 Sosok
         bool seq4Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq4Sosok", 
             () => { seq4Complete = true; });
@@ -103,7 +82,6 @@ public class NarratorDay6 : NarratorBase
         
         yield return new WaitForSeconds(1f);
         
-        // Seq5 Mendekat
         bool seq5Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq5Mendekat", 
             () => { seq5Complete = true; });
@@ -111,15 +89,14 @@ public class NarratorDay6 : NarratorBase
         
         yield return new WaitForSeconds(1f);
         
-        // Mother comes to help
         yield return StartCoroutine(MoveAgentToMovementPosition(CharacterType.Mother, 0));
         
-        // Seq6 Khawatir
         bool seq6Complete = false;
         dialogGameManager.StartCoreGame("GameData/Dialog/Day6/Seq6Khawatir", 
             () => { seq6Complete = true; });
         yield return new WaitUntil(() => seq6Complete);
         
+        yield return new WaitForSeconds(1f);
         FadeCloseEyes();
         yield return new WaitForSeconds(2f);
         
